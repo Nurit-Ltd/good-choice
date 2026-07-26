c418540750f4f89b93d90a9345c73e47271ad906
Al Amin <alamincoders@gmail.com>
2026-07-26 06:37:14 +0600

    feat: add Experiences section to homepage with custom navigation arrows and iconography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Banner } from "@/components/features/home/Banner";
 import { Collections } from "@/components/features/home/Collections";
+import { Experiences } from "@/components/features/home/Experiences";
 import { Explore } from "@/components/features/home/Explore";
 import { MadeFurniture } from "@/components/features/home/MadeFurniture";
 import { RecentlyCrafted } from "@/components/features/home/RecentlyCrafted";
@@ -30,6 +31,9 @@ export default async function Home() {
 
       {/* Explore Elevated Living Essentials Section */}
       <Explore products={products} />
+
+      {/* Crafted Experiences Section */}
+      <Experiences />
     </div>
   );
 }
